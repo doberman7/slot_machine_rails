3349cb91a72553266a12130c7066e0598bd4dd07
doberman7 <ivanrubioangeles@gmail.com>
2017-08-04 22:57:41 -0500

    destroyed machine controller

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -3,4 +3,14 @@ class RollsController < ApplicationController
   def create
     @roll = Roll.new
   end
+
+  def index
+    @rolls = []
+    p "/" * 50
+    3.times { @rolls << Roll.create }
+    #Mostrar "WINER" si al mapear Rolls, solo hay un unico value
+    p @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
+    p "/" * 50
+    render 'rolls/index'
+  end
 end
--- test/controllers/machine_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MachineControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get machine_index_url
-    assert_response :success
-  end
-
-end
