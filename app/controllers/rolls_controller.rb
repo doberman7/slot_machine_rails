5d656ca1fa43ec7f2932b500c665a926de167e77
doberman7 <ivanrubioangeles@gmail.com>
2017-08-06 15:06:47 -0500

    funcional sin Ajax, se uso verbo GET debido a un error ActionController::InvalidAuthenticityToken

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,16 +1,39 @@
 class RollsController < ApplicationController
 
-  def create
-    @roll = Roll.new
-  end
+  # def create
+  #   @roll = Roll.new
+  # end
 
   def index
     @rolls = []
-    p "/" * 50
+    #p "/" * 50
     3.times { @rolls << Roll.create }
     #Mostrar "WINER" si al mapear Rolls, solo hay un unico value
-    p @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
-    p "/" * 50
+    @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
+    #p "/" * 50
+    render 'rolls/index'
+  end
+
+  def roll_dice
+
+    @rolls = []
+
+    if params[:value]
+        3.times { @rolls << Roll.create({ value: value }) }
+      else
+        3.times { @rolls << Roll.create }
+    end
+    #Se dejan solo los valores de cada Roll en el Ary, se eliminan los repetidos, y si al la cuenta de elementos dentro del Ary es 1, @win es "WINER"
+    @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
+        
+    # if request.xhr?
+    #
+    #     # renderear :_die_roll, pero no layout:
+    #     render 'die_roll', layout: false
+    #   else
+    #
+    #     render 'index'
+    # end
     render 'rolls/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get 'machine/index'
   root 'rolls#index'
-
+  get '/rolls' => 'rolls#roll_dice'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
