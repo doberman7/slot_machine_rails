a0d02352a65a16ebdb5a286f030d31a8549990d8
doberman7 <ivanrubioangeles@gmail.com>
2017-08-08 18:25:08 -0500

    ajax slot machine

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,11 +1,12 @@
 class RollsController < ApplicationController
 
-  # def create
-  #   @roll = Roll.new
-  # end
+  def create
+    @roll = Roll.new
+  end
 
   def index
     @rolls = []
+
     #p "/" * 50
     3.times { @rolls << Roll.create }
     #Mostrar "WINER" si al mapear Rolls, solo hay un unico value
@@ -20,19 +21,28 @@ class RollsController < ApplicationController
 
     if params[:value]
         3.times { @rolls << Roll.create({ value: value }) }
+        p "+" * 50
+        p 'entro'
+        p "+" * 50
       else
         3.times { @rolls << Roll.create }
+        p "+" * 50
+        p 'NO entro'
+        p "+" * 50
     end
     #Se dejan solo los valores de cada Roll en el Ary, se eliminan los repetidos, y si al la cuenta de elementos dentro del Ary es 1, @win es "WINER"
     @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
 
     if request.xhr?
-        # renderear vista parcial:_die_roll, pero no layout:
-        render '_die_roll', layout: false
-        p " ajax " * 50
+        # el metodo desponde a estos formatos
+        respond_to do |format|
+          format.html
+          format.js
+        end
+
       else
         render 'index'
-        p "no ajax " * 50
+
     end
     #render 'rolls/index'
   end
